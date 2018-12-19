ffa3d592c1c84fa58d966476d40558c85d140380
kalkairis <kalka.iris@gmail.com>
2018-12-19 17:33:27 +0200

    adding questions for people of interest

--- src/load_data.py
+++ src/load_data.py
@@ -8,7 +8,7 @@ def join_data_frames():
     metaphlan_abundances = pd.read_pickle(join(BaseConfig.data_dir, 'MPA.dat'))
     metaphlan_abundances.transpose(inplace=True)
     metaphlan_abundances['FD'] = tmp['FD'] = list(map(lambda x: x.split('_')[0], metaphlan_abundances.index.values))
-    
+
     stool_metaphlan_df = pd.read_pickle(join(BaseConfig.data_dir, 'StoolMetadataDF.dat'))
     connections_df = pd.read_pickle(join(BaseConfig.data_dir, 'ConnectionsDF.dat'))
     print('bla')
