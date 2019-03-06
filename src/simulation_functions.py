b08b5eb31128dbcaf7ed698542f4fc39905cfa45
kalkairis <kalka.iris@gmail.com>
2019-03-06 12:00:41 +0200

    Move treatment computing functions to .py

--- src/simulation_functions.py
+++ src/simulation_functions.py
@@ -42,21 +42,21 @@ def kang_shafer_data(size=(1,), random_seed=0, X_observed=True):
     X4 = np.random.normal(0, 1, size=size)
 
     true_propensity = 1 / (1 + np.exp(X1 - 0.5 * X2 + 0.25 * X3 + 0.1 * X4))
-    A = (true_propensity >= 0.5).astype(int)
+    t = (true_propensity >= 0.5).astype(int)
     # print(X1, X2, X3, X4, true_propensity, A)
 
-    E_Y = 210 + A + 27.4 * X1 + 13.78 * X2 + 13.7 * X3 + 13.7 * X4
+    E_Y = 210 + t + 27.4 * X1 + 13.78 * X2 + 13.7 * X3 + 13.7 * X4
 
-    Y_true = np.random.normal(E_Y, 1)
+    y = np.random.normal(E_Y, 1)
 
     if X_observed:
-        return X1, X2, X3, X4, A, Y_true
+        return X1, X2, X3, X4, t, y
     else:  # Xs are "not observed" , but a nonlinear transformation of them is
         X1_t = np.exp(X1 / 2)
         X2_t = (X2 / (1 + np.exp(X1))) + 10
         X3_t = (((X1 * X3) / 25) + 0.6) ** 3
         X4_t = (X2 + X4 + 20) ** 2
-        return X1_t, X2_t, X3_t, X4_t, A, Y_true
+        return X1_t, X2_t, X3_t, X4_t, t, y
 
 
 def get_ACIC_2019_datasets(high_dim=True, test=True):
